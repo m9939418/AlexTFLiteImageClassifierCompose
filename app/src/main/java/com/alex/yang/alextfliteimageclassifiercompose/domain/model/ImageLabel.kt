661b1fbbe2d2420fef527dfa87e06bc6a039b53f
Alex <m9939418@gmail.com>
2025-12-09 12:54:47 +0800

    Add Domain

---
+++ app/src/main/java/com/alex/yang/alextfliteimageclassifiercompose/domain/model/ImageLabel.kt
@@ -0,0 +1,11 @@
+package com.alex.yang.alextfliteimageclassifiercompose.domain.model
+
+/**
+ * Created by AlexYang on 2025/12/8.
+ *
+ *
+ */
+data class ImageLabel(
+    val label: String,
+    val score: Float
+)
